1841927f51eecbcd9e41582299a79c58da33eb03
Pranav Harish Nathani <pranav.nathani@binus.ac.id>
2023-09-21 18:42:54 +0700

    Update ASSIGNMENT1.py

--- ASSIGNMENT1.py
+++ ASSIGNMENT1.py
@@ -1,24 +1,29 @@
-x = float(input("Enter the first value: "))
-calc = input("Enter the logical operator: ")
-y = float(input("Enter the second value: "))
 
-if type(x) == float & type(y) == float:
-    if calc == "+":
-        print(x + y)
-    elif calc == "-":
-        print(x - y)
-    elif calc == "*":
-        print(x * y)
-    elif calc == "/":
-        if y != 0:
-            print(x / y)
+x = input("Enter the first value: ")
+calc = input("Enter the logical operator: ")
+y = input("Enter the second value: ")
+
+try:
+    x=float(x)
+    y = float(y)
+
+    if type(x,y) == float:
+        if calc == "+":
+            print(x + y)
+        elif calc == "-":
+            print(x - y)
+        elif calc == "*":
+            print(x * y)
+        elif calc == "/":
+            if y != 0:
+                print(x / y)
+            else:
+                print("Division by zero is not allowed.")
+        elif calc == "**":
+            print(x ** y)
         else:
-            print("Division by zero is not allowed.")
-    elif calc == "**":
-        print(x ** y)
-    else:
-        print("Wrong logical operator")
-else:
-    print("enter proper values")
-    
+            print("Wrong logical operator")
+
+except ValueError:
+    print("enter a numeric value")
 
