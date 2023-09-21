42a03ffeefe08121813501455ad8f46afa964238
Pranav Harish Nathani <pranav.nathani@binus.ac.id>
2023-09-21 18:05:57 +0700

    Add files via upload

---
+++ ASSIGNMENT1.py
@@ -0,0 +1,24 @@
+x = float(input("Enter the first value: "))
+calc = input("Enter the logical operator: ")
+y = float(input("Enter the second value: "))
+
+if type(x) == float | type(y) == float:
+    if calc == "+":
+        print(x + y)
+    elif calc == "-":
+        print(x - y)
+    elif calc == "*":
+        print(x * y)
+    elif calc == "/":
+        if y != 0:
+            print(x / y)
+        else:
+            print("Division by zero is not allowed.")
+    elif calc == "**":
+        print(x ** y)
+    else:
+        print("Wrong logical operator")
+else:
+    print("enter proper values")
+    
+
