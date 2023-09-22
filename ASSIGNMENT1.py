2df2e71e069bf8fc89d3231b67e3b0a26f7dc34d
Pranav Harish Nathani <pranav.nathani@binus.ac.id>
2023-09-23 01:57:48 +0700

    Update ASSIGNMENT1.py

--- ASSIGNMENT1.py
+++ ASSIGNMENT1.py
@@ -1,31 +1,43 @@
 while True:
-    x,calc,y = input("Enter your problem(ex: 3 * 3 - use a space)(for bmi : 52.6 b 1.72) height must be in meters: ").split(" ")
+    calculator = input("would u like to use math/bmi calculator: ")
 
-    try:
-        x = float(x)
-        y = float(y)
+    if calculator == "math":
+        print("WELCOME TO MATH CALCULATOR")
+        x,calc,y = input("Enter your problem(ex: 3 * 3 - use a space): ").split(" ")
+
+        try:
+            x = float(x)
+            y = float(y)
 
 
-        
-        if calc == "+":
-            print(f"{x} + {y} = ",x+y )
-        elif calc == "-":
-            print(f"{x} - {y} = ",x - y)
-        elif calc == "*":
-            print(f"{x} * {y} = ",x * y)
-        elif calc == "/":
-            if y != 0:
-                print(f"{x} / {y} = ",x / y)
+            
+            if calc == "+":
+                print(f"{x} + {y} = ",x+y )
+            elif calc == "-":
+                print(f"{x} - {y} = ",x - y)
+            elif calc == "*":
+                print(f"{x} * {y} = ",x * y)
+            elif calc == "/":
+                if y != 0:
+                    print(f"{x} / {y} = ",x / y)
+                else:
+                    print("Division by zero is not allowed.")
+            elif calc == "**":
+                print(f"{x} ** {y} = ",x ** y)
             else:
-                print("Division by zero is not allowed.")
-        elif calc == "**":
-            print(f"{x} ** {y} = ",x ** y)
-        elif calc == "bmi":
-            print(f"{x}/({y} * {y}) = ",x/ y**2)
-        else:
-            print("Wrong logical operator")
+                print("Wrong logical operator")
+                break
+        except ValueError:
+            print("enter a numeric value")  
+    elif calculator == "bmi":
+        print("WELCOME TO BMI CALCULATOR")
+        weight,height = input("Enter your weight(kg) and height(m) seperated by a space: ").split(" ")
 
+        try:
+            weight = float(weight)
+            height = float(height)
+            
+            print(f"{weight}/({height} * {height}) = ",weight/ height**2)
 
-    except ValueError:
-        print("enter a numeric value")                                                                                       
-        
+        except ValueError: 
+             print()                                               
