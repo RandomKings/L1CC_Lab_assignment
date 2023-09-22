b4bb6e3eea326fb29d62b78d3918ed53fbed7459
Pranav Harish Nathani <pranav.nathani@binus.ac.id>
2023-09-22 09:44:37 +0700

    Update ASSIGNMENT1.py

--- ASSIGNMENT1.py
+++ ASSIGNMENT1.py
@@ -1,25 +1,29 @@
-x,calc,y = input("Enter your problem(ex: 3 * 3 - use a space) : ").split(" ")
+while True:
+    x,calc,y = input("Enter your problem(ex: 3 * 3 - use a space): ").split(" ")
 
-try:
-    x = float(x)
-    y = float(y)
+    try:
+        x = float(x)
+        y = float(y)
 
 
-    if calc == "+":
-        print(x + y)
-    elif calc == "-":
-        print(x - y)
-    elif calc == "*":
-        print(x * y)
-    elif calc == "/":
-        if y != 0:
-            print(x / y)
+        
+        if calc == "+":
+            print(f"{x} + {y} = ",x+y )
+        elif calc == "-":
+            print(f"{x} - {y} = ",x - y)
+        elif calc == "*":
+            print(f"{x} * {y} = ",x * y)
+        elif calc == "/":
+            if y != 0:
+                print(f"{x} / {y} = ",x / y)
+            else:
+                print("Division by zero is not allowed.")
+        elif calc == "**":
+            print(f"{x} ** {y} = ",x ** y)
         else:
-            print("Division by zero is not allowed.")
-    elif calc == "**":
-        print(x ** y)
-    else:
-        print("Wrong logical operator")
+            print("Wrong logical operator")
 
-except ValueError:
-    print("enter a numeric value")
+
+    except ValueError:
+        print("enter a numeric value")
+        
